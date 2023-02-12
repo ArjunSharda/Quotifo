92ef654eb2ce0da50f56a3fb1cc3c57a6fc703c2
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2023-02-12 11:14:48 -0600

    Update quote.js

--- docs/quote.js
+++ docs/quote.js
@@ -1,3 +1,3 @@
-// Gets the quote based on the number you choose (1 will return the first quote)
+// Gets the quote based on the number you choose (1 will return the first quote) and returns it
 const quotifo = require('quotifo');
 console.log(quotifo.quote(1))
